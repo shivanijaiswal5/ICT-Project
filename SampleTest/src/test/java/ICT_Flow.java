7585220aee1fbc822f7c25496fd8ea984385245a
shivani.jaiswal <shivani.jaiswal@VHCOLPT-HH-129.vibgyorschools.com>
2025-03-24 16:12:54 +0530

    update the changes

--- SampleTest/src/test/java/ICT_Flow.java
+++ SampleTest/src/test/java/ICT_Flow.java
@@ -1,6 +1,16 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,7 +33,7 @@ public class ICT_Flow {
 
 	@BeforeTest
 	public static void getURL() {
-		driver.get("https://ict-recurring-uat.web.app/");
+		driver.get("https://lms-frontend-803019860386.asia-south1.run.app/lesson-plan/");
 	}
 
 	@BeforeTest	
@@ -34,13 +44,50 @@ public class ICT_Flow {
     
 	@Test
 	public static void loginPage() {
-		driver.findElement(By.xpath("//input[@id='Email Address']")).sendKeys("sufi.sawant@gmail.com");
-		driver.findElement(By.xpath("//input[@id='Email Address']//following::input[1]")).sendKeys("Ram@1234");
+		driver.findElement(By.xpath("//*[@id=\":r7:-label\"]")).sendKeys("teacher1@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\":r8:-label\"]")).sendKeys("Test@123");
 		timeout();
-		driver.findElement(By.xpath("//button//following::button[1]")).click();
+		driver.findElement(By.xpath("//*[@id=\"__next\"]/div[1]/div[2]/main/div/div[2]/div/div/form/div[2]/div[3]/label/span[1]/input")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"__next\"]/div[1]/div[2]/main/div/div[2]/div/div/form/div[2]/button")).click();
+		
+		
+		
 		SoftAssert sa = new SoftAssert();
 		sa.assertEquals("", null);
 	}
+	
+	@Test
+	public static ArrayList<String> getData()
+	{
+		
+		String filepath = "";
+		ArrayList<String> links = new ArrayList<String>();
+		  
+	        try {
+	        		FileInputStream fis = new FileInputStream(new File(filepath));
+	            // Create a workbook instance (for .xlsx files)
+	            Workbook workbook = new XSSFWorkbook(fis);
+	            
+	            // Get the first sheet from the workbook
+	            Sheet sheet = workbook.getSheetAt(0);
+	            
+	            // Iterate through each row in the sheet
+	            for (Row row : sheet) {
+	                
+	                // Iterate through each cell in the row and add only string values
+	                for (Cell cell : row) {
+	                    if (cell.getCellType() == CellType.STRING) {
+	                        String cellValue = cell.getStringCellValue();
+	                        links.add(cellValue);
+	                    }
+	                }}
+	        return links;
+    } catch (IOException e) {
+        e.printStackTrace();
+        return links;
+    }}
+	            
     
      @AfterTest
      public static void closeDriver()
